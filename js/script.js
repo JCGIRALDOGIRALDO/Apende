b7a6f8826fc2777ce6ad24dab4cf18de266283b4
Jonathan Smith <jonathan.pulgarin1994@gmail.com>
2025-06-20 04:06:39 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,10 @@
-let verdadero = true;
-let edad = 16;
+const imageElement = document.getElementById('displayedImage');
 
-let arreglo = [1, 2, 3, 4, 5];
-
-// operadores lógicos
-// variables
-// tipo de datos primitivos
-
-function calculeEdad() {
-  if (edad >= 18) {
-    console.log("Es mayor de edad");
-  } else {
-    console.log("Es menor de edad");
-  }
-}
-
-console.log(calculeEdad());
-
-for (let index = 0; index < arreglo.length; index++) {
-  const como = arreglo[index];
-  console.log(como);
-}
+document.querySelectorAll('a[data-img]').forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const imageSrc = link.getAttribute('data-img');
+    imageElement.src = imageSrc;
+    imageElement.style.display = 'block';
+  });
+});
